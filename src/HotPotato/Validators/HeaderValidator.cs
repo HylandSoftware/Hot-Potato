d2a4b5bd56e89b886384bf9d8c8ce32649be1bf7
Tim Cosgriff <timothy.cosgriff@hyland.com>
2018-12-28 13:42:57 -0500

    Adding test for HeaderValidator. Adding hack to convert header value to JSON because JsonSchema requires it.

--- src/HotPotato/Validators/HeaderValidator.cs
+++ src/HotPotato/Validators/HeaderValidator.cs
@@ -1,5 +1,6 @@
 ﻿using HotPotato.Http;
 using HotPotato.Results;
+using Newtonsoft.Json;
 using NJsonSchema.Validation;
 using NSwag;
 using System;
@@ -10,32 +11,39 @@ namespace HotPotato.Validators
 {
     public class HeaderValidator
     {
-        public IEnumerable<Result> Validate(HttpHeaders headers, SwaggerHeaders schema)
+        public ICollection<Result> Validate(HttpHeaders headers, SwaggerHeaders schema)
         {
             _ = headers ?? throw new ArgumentNullException(nameof(headers));
             _ = schema ?? throw new ArgumentNullException(nameof(schema));
 
+            List<Result> results = new List<Result>();
             foreach (var item in schema)
             {
                 string headerKey = item.Key;
                 if (!headers.ContainsKey(headerKey))
                 {
-                    yield return ResultFactory.HeaderNotFoundResult(headerKey);
+                    results.Add(ResultFactory.HeaderNotFoundResult(headerKey));
                 }
-                List<string> headerValues = headers[headerKey];
-                foreach (string value in headerValues)
+                else
                 {
-                    ICollection<ValidationError> result = item.Value.Validate(value);
-                    if (result == null || result.Count == 0)
+                    List<string> headerValues = headers[headerKey];
+                    foreach (string value in headerValues)
                     {
-                        yield return ResultFactory.HeaderValidResult(headerKey, value);
+                        // HACK - Need to convert to JSON because that's how NJsonSchema likes it.
+                        string jValue = JsonConvert.SerializeObject(value);
+                        ICollection<ValidationError> result = item.Value.Validate(jValue);
+                        if (result == null || result.Count == 0)
+                        {
+                            results.Add(ResultFactory.HeaderValidResult(headerKey, value));
+                        }
+                        else
+                        {
+                            results.Add(ResultFactory.HeaderInvalidResult(headerKey, value, result));
+                        }
                     }
-                    else
-                    {
-                        yield return ResultFactory.HeaderInvalidResult(headerKey, value, result);
-                    }
-                }
-            }
+
+                }            }
+            return results;
         }
     }
 }
---
+++ test/HotPotato.Test/Validators/HeaderValidatorTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using HotPotato.Http;
+using NSwag;
+using NJsonSchema;
+using HotPotato.Results;
+
+namespace HotPotato.Validators
+{
+    public class HeaderValidatorTest
+    {
+        private const string AValidHeaderKey = "X-Header-Key";
+        private const string AValidHeaderValue = "value";
+        private const string AValidSchema = @"{'type': 'integer'}";
+        private const string AnInvalidValue = "invalidValue";
+
+        [Fact]
+        public void Validate_KeyNotFound()
+        {
+            HttpHeaders headers = new HttpHeaders();
+            SwaggerHeaders schema = new SwaggerHeaders();
+            schema.Add(AValidHeaderKey, new JsonSchema4());
+            HeaderValidator subject = new HeaderValidator();
+
+            ICollection<Result> result = subject.Validate(headers, schema);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            IEnumerator<Result> enumerator = result.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.IsAssignableFrom<HeaderNotFoundResult>(enumerator.Current);
+        }
+
+        [Fact]
+        public void Validate_ValidSchema()
+        {
+            HttpHeaders headers = new HttpHeaders();
+            headers.Add(AValidHeaderKey, AValidHeaderValue);
+            JsonSchema4 jsonSchema = JsonSchema4.CreateAnySchema();
+            SwaggerHeaders schema = new SwaggerHeaders();
+            schema.Add(AValidHeaderKey, jsonSchema);
+            HeaderValidator subject = new HeaderValidator();
+
+            ICollection<Result> result = subject.Validate(headers, schema);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            IEnumerator<Result> enumerator = result.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.IsAssignableFrom<HeaderValidResult>(enumerator.Current);
+        }
+
+        [Fact]
+        public void Validate_InvalidSchema()
+        {
+            HttpHeaders headers = new HttpHeaders();
+            headers.Add(AValidHeaderKey, AnInvalidValue);
+            JsonSchema4 jsonSchema = JsonSchema4.FromJsonAsync(AValidSchema).Result;
+            SwaggerHeaders schema = new SwaggerHeaders();
+            schema.Add(AValidHeaderKey, jsonSchema);
+            HeaderValidator subject = new HeaderValidator();
+
+            ICollection<Result> result = subject.Validate(headers, schema);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            IEnumerator<Result> enumerator = result.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.IsAssignableFrom<HeaderInvalidResult>(enumerator.Current);
+        }
+    }
+}
