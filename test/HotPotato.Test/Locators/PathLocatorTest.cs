99d50c447a11f016caa353a5649f8e6997b23d81
David Billie <dbillie@onbase.net>
2019-02-06 10:08:14 -0500

    PathMatcher rework, PathLocator unit tests, test specs

---
+++ test/HotPotato.Test/Locators/PathLocatorTest.cs
@@ -0,0 +1,52 @@
+﻿using HotPotato.Http.Default;
+using HotPotato.Models;
+using NSwag;
+using static NSwag.SwaggerYamlDocument;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HotPotato.OpenApi.Locators.NSwag
+{
+    public class PathLocatorTest
+    {
+        [Fact]
+        public void PathLocator_LocatesWithParam()
+        {
+            string endpointWithPar = "http://api.docs.hyland.io/keyword/keyword-type-groups/48732/keyword-types";
+            HttpRequest testRequest = new HttpRequest(HttpMethod.Get, new Uri(endpointWithPar));
+            HttpResponse testResponse = new HttpResponse(HttpStatusCode.OK, null);
+            HttpPair testPair = new HttpPair(testRequest, testResponse);
+
+            string path = Path.Combine(Environment.CurrentDirectory, @"specs\keyword\", "specification.yaml");
+            Task<SwaggerDocument> swagTask = FromFileAsync(path);
+            SwaggerDocument swagDoc = swagTask.Result;
+
+            PathLocator pathLoc = new PathLocator();
+            SwaggerPathItem subject = pathLoc.Locate(testPair, swagDoc);
+            Assert.NotNull(subject);
+        }
+
+        [Fact]
+        public void PathLocator_LocatesWithoutParam()
+        {
+            string endpointWithoutPar = "https://api.hyland.com/workflow/life-cycles";
+            HttpRequest testRequest = new HttpRequest(HttpMethod.Get, new Uri(endpointWithoutPar));
+            HttpResponse testResponse = new HttpResponse(HttpStatusCode.OK, null);
+            HttpPair testPair = new HttpPair(testRequest, testResponse);
+
+            string path = Path.Combine(Environment.CurrentDirectory, @"specs\workflow\", "specification.yaml");
+            Task<SwaggerDocument> swagTask = FromFileAsync(path);
+            SwaggerDocument swagDoc = swagTask.Result;
+
+            PathLocator pathLoc = new PathLocator();
+            SwaggerPathItem subject = pathLoc.Locate(testPair, swagDoc);
+            Assert.NotNull(subject);
+        }
+
+    }
+
+}
