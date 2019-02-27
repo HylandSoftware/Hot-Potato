51f818de10b61b265d9501af61d55bc9f3901ff2
Cameron Miller <Cameron.Miller@hyland.com>
2019-02-27 15:12:23 -0500

    Added interface for IResultCollector

---
+++ src/HotPotato.OpenApi/Results/IResultCollector.cs
@@ -0,0 +1,9 @@
+﻿using HotPotato.Core.Models;
+
+namespace HotPotato.OpenApi.Results
+{
+    public interface IResultCollector
+    {
+        void WriteResults(HttpPair pair, Result result);
+    }
+}
