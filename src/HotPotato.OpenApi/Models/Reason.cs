c0a9f97c3826b1c84d4c43f882377ce82fdb898c
Cameron Miller <Cameron.Miller@hyland.com>
2019-03-06 11:48:25 -0500

    removed valid reasons since we dont care why it passed

--- src/HotPotato.OpenApi/Models/Reason.cs
+++ src/HotPotato.OpenApi/Models/Reason.cs
@@ -3,15 +3,12 @@
     public enum Reason
     {
         Unknown,
-        ValidBody,
         InvalidBody,
-        ValidHeaders,
         InvalidHeaders,
         MissingHeaders,
         MissingPath,
         MissingMethod,
         MissingStatusCode,
-        ExpectedNoBody,
         UnexpectedBody
     }
 }
