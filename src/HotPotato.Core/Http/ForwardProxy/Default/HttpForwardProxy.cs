20b6e1e2f3775b01c00a072354f5a3d7dd5848ec
rmacaleese <richard.macaleese@hyland.com>
2019-05-14 15:14:24 -0400

    Added support for a forward proxy configured in appsettings.json

---
+++ src/HotPotato.Core/Http/ForwardProxy/Default/HttpForwardProxy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace HotPotato.Core.Http.ForwardProxy.Default
+{
+    public class HttpForwardProxy : IWebProxy
+    {
+        private ILogger Logger { get; set; }
+        private Uri ProxyUri { get; set; }
+        private bool ByPassOnLocal { get; set; }
+
+        public HttpForwardProxy(HttpForwardProxyConfig configuration, ILogger<HttpForwardProxy> logger)
+        {
+            this.Logger = logger;
+            //here you can load it from your custom config settings 
+            try
+            {
+                this.ProxyUri = new Uri(configuration.proxyAddress);
+            }
+            catch (ArgumentNullException)
+            {
+                logger.LogError("ForwardProxy is enabled but no proxyAddress was provided. Bypassing proxy.");
+            }
+            catch (UriFormatException)
+            {
+                logger.LogError("ForwardProxy is enabled but the value provided for proxyAddress cannot be convert to a URI. Bypassing proxy.");
+            }
+        }
+
+        public ICredentials Credentials { get; set; }
+
+        public Uri GetProxy(Uri destination)
+        {
+            return this.ProxyUri;
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            //you can proxy all requests or implement bypass urls based on config settings
+            return false;
+        }
+    }
+}
---
+++ src/HotPotato.Core/Http/ForwardProxy/HttpForwardProxyConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotPotato.Core.Http.ForwardProxy
+{
+    public class HttpForwardProxyConfig
+    {
+        public bool enabled { get; set; }
+        public string proxyAddress { get; set; }
+        public bool bypassOnLocal { get; set; }
+    }
+}
