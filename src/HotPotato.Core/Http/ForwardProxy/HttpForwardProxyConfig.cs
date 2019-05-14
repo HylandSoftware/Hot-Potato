4470e097d1ad2b25edbf17266fcb0402c0c76a1e
rmacaleese <richard.macaleese@hyland.com>
2019-05-14 15:37:54 -0400

    Fixed spacing in appsettings and capitalization of proxy settings

--- src/HotPotato.Core/Http/ForwardProxy/HttpForwardProxyConfig.cs
+++ src/HotPotato.Core/Http/ForwardProxy/HttpForwardProxyConfig.cs
@@ -6,8 +6,8 @@ namespace HotPotato.Core.Http.ForwardProxy
 {
     public class HttpForwardProxyConfig
     {
-        public bool enabled { get; set; }
-        public string proxyAddress { get; set; }
-        public bool bypassOnLocal { get; set; }
+        public bool Enabled { get; set; }
+        public string ProxyAddress { get; set; }
+        public bool BypassOnLocal { get; set; }
     }
 }
