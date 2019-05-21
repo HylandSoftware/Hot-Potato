2c5bf9f20f9226e02447f95a1c640455e48d8ec9
David Billie <dbillie@onbase.net>
2019-05-21 13:06:35 -0400

    Separate test classes for each BodyValidator, BodyValidatorFactory tests

---
+++ test/HotPotato.OpenApi.Test/Validators/BodyValidator/BodyValidatorFactoryTest.cs
@@ -0,0 +1,36 @@
+﻿using HotPotato.Core.Http;
+using Xunit;
+
+namespace HotPotato.OpenApi.Validators
+{
+    public class BodyValidatorFactoryTest
+    {
+        private const string AValidJsonBody = "{'foo': '1'}";
+        private const string AValidTextBody = "Content";
+        private const string AValidXmlBody = @"<LGNotification><MediaType>video</MediaType><StatusFlag>new</StatusFlag><URL>http://domain.com/program/app?clienttype=htmlamp;id=49977</URL></LGNotification>";
+
+        [Fact]
+        public void BodyValidatorFactory_Create_ReturnsJson()
+        {
+            BodyValidator result = BodyValidatorFactory.Create(AValidJsonBody, new HttpContentType("application/json"));
+
+            Assert.IsType<JsonBodyValidator>(result);
+        }
+
+        [Fact]
+        public void BodyValidator_Create_ReturnsText()
+        {
+            BodyValidator result = BodyValidatorFactory.Create(AValidTextBody, new HttpContentType("text/plain"));
+
+            Assert.IsType<TextBodyValidator>(result);
+        }
+
+        [Fact]
+        public void BodyValidator_Create_ReturnsXml()
+        {
+            BodyValidator result = BodyValidatorFactory.Create(AValidXmlBody, new HttpContentType("application/xml"));
+
+            Assert.IsType<XmlBodyValidator>(result);
+        }
+    }
+}
--- test/HotPotato.OpenApi.Test/Validators/BodyValidator/JsonBodyValidatorTest.cs
+++ test/HotPotato.OpenApi.Test/Validators/BodyValidator/JsonBodyValidatorTest.cs
@@ -6,34 +6,33 @@ using Xunit;
 
 namespace HotPotato.OpenApi.Validators
 {
-    public class BodyValidatorTest
+    public class JsonBodyValidatorTest
     {
         private const string AValidBody = "{'foo': '1'}";
         private const string AnInvalidBody = "{'foo': 'abc'}";
         private const string AValidSchema = @"{'type': 'integer'}";
-        private const string AValidXmlBody = @"<LGNotification><MediaType>video</MediaType><StatusFlag>new</StatusFlag><URL>http://domain.com/program/app?clienttype=htmlamp;id=49977</URL></LGNotification>";
 
         [Fact]
-        public void BodyValidator_ReturnsTrueWithValid()
+        public void JsonBodyValidator_ReturnsTrueWithValid()
         {
             JsonSchema4 schema = JsonSchema4.CreateAnySchema();
             SwaggerResponse swagResp = new SwaggerResponse();
             swagResp.ActualResponse.Schema = schema;
 
-            BodyValidator subject = BodyValidatorFactory.Create(AValidBody, new HttpContentType("application/json"));
+            JsonBodyValidator subject = new JsonBodyValidator(AValidBody, new HttpContentType("application/json"));
             IValidationResult result = subject.Validate(swagResp);
 
             Assert.True(result.Valid);
         }
 
         [Fact]
-        public void BodyValidator_ReturnsFalseWithInvalid()
+        public void JsonBodyValidator_ReturnsFalseWithInvalid()
         {
             JsonSchema4 schema = JsonSchema4.FromJsonAsync(AValidSchema).Result;
             SwaggerResponse swagResp = new SwaggerResponse();
             swagResp.ActualResponse.Schema = schema;
 
-            BodyValidator subject = BodyValidatorFactory.Create(AnInvalidBody, new HttpContentType("application/json"));
+            JsonBodyValidator subject = new JsonBodyValidator(AnInvalidBody, new HttpContentType("application/json"));
             InvalidResult result = (InvalidResult)subject.Validate(swagResp);
 
             Assert.False(result.Valid);
@@ -42,13 +41,13 @@ namespace HotPotato.OpenApi.Validators
         }
 
         [Fact]
-        public void BodyValidator_ReturnsFalseWithNullBody()
+        public void JsonBodyValidator_ReturnsFalseWithNullBody()
         {
             JsonSchema4 schema = JsonSchema4.CreateAnySchema();
             SwaggerResponse swagResp = new SwaggerResponse();
             swagResp.ActualResponse.Schema = schema;
 
-            BodyValidator subject = BodyValidatorFactory.Create(null, new HttpContentType("application/json"));
+            JsonBodyValidator subject = new JsonBodyValidator(null, new HttpContentType("application/json"));
             InvalidResult result = (InvalidResult)subject.Validate(swagResp);
 
             Assert.False(result.Valid);
@@ -56,40 +55,16 @@ namespace HotPotato.OpenApi.Validators
         }
 
         [Fact]
-        public void BodyValidator_ReturnsFalseWithNullSchema()
+        public void JsonBodyValidator_ReturnsFalseWithNullSchema()
         {
             SwaggerResponse swagResp = new SwaggerResponse();
             swagResp.ActualResponse.Schema = null;
 
-            BodyValidator subject = BodyValidatorFactory.Create(AValidBody, new HttpContentType("application/json"));
+            BodyValidator subject = new JsonBodyValidator(AValidBody, new HttpContentType("application/json"));
             InvalidResult result = (InvalidResult)subject.Validate(swagResp);
 
             Assert.False(result.Valid);
             Assert.Equal(Reason.MissingSpecBody, result.Reason);
-        }
-
-        [Fact]
-        public void BodyValidator_ReturnsTrueWithPlainText()
-        {
-            JsonSchema4 schema = JsonSchema4.CreateAnySchema();
-            SwaggerResponse swagResp = new SwaggerResponse();
-            swagResp.ActualResponse.Schema = schema;
-
-            BodyValidator subject = BodyValidatorFactory.Create("Content", new HttpContentType("text/plain"));
-
-            Assert.True(subject.Validate(swagResp).Valid);
-        }
-
-        [Fact]
-        public void BodyValidator_ReturnsTrueWithXml()
-        {
-            JsonSchema4 schema = JsonSchema4.CreateAnySchema();
-            SwaggerResponse swagResp = new SwaggerResponse();
-            swagResp.ActualResponse.Schema = schema;
-
-            BodyValidator subject = BodyValidatorFactory.Create(AValidXmlBody, new HttpContentType("application/xml"));
-
-            Assert.True(subject.Validate(swagResp).Valid);
         }
     }
 }
