365216711cd0156f485cde09969d52be91274458
Cameron Miller <Cameron.Miller@hyland.com>
2019-06-03 14:46:22 -0400

    Initial setup of API for testing

---
+++ HotPotato.Api/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotPotato.Api.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Price { get; set; }
+        public string[] Items { get; set; }
+    }
+}
