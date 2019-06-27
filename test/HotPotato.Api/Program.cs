240a5bc3d8e8d668f82e49dd24f75825009ae7bd
Cameron Miller <Cameron.Miller@hyland.com>
2019-06-27 12:31:33 -0400

    adding kestrel and useUrls()

--- test/HotPotato.Api/Program.cs
+++ test/HotPotato.Api/Program.cs
@@ -19,6 +19,8 @@ namespace HotPotato.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000");
     }
 }
