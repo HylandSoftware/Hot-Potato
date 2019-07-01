8a0f35beab6781f7042a8de65c6b73ea019bdd58
David Billie <dbillie@onbase.net>
2019-07-01 12:04:11 -0400

    Using Moq to set up HttpMessageHandler, removing ref to RichardSzalay

--- test/HotPotato.Core.Test/Http/Default/HttpClientTest.cs
+++ test/HotPotato.Core.Test/Http/Default/HttpClientTest.cs
@@ -1,8 +1,11 @@
-﻿using RichardSzalay.MockHttp;
+﻿using static HotPotato.Core.CoreTestMethods;
+using Moq;
+using Moq.Protected;
 using System;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using Xunit;
 
 namespace HotPotato.Core.Http.Default
@@ -21,16 +24,25 @@ namespace HotPotato.Core.Http.Default
         [Fact]
         public async void SendAsync_ExecutesRequest()
         {
-            var mockHttp = new MockHttpMessageHandler();
-            mockHttp.When(AValidEndpoint)
-                .Respond(HttpStatusCode.OK);
-            using (HttpRequest request = new HttpRequest(HttpMethod.Get, new Uri(AValidEndpoint)))
+            var handlerMock = GetMockHandler(HttpStatusCode.OK);
+            HttpMessageHandler mockHandler = handlerMock.Object;
+
+            Uri endpointUri = new Uri(AValidEndpoint);
+
+            using (HttpRequest request = new HttpRequest(HttpMethod.Get, endpointUri))
             {
-                HttpClient subject = new HttpClient(mockHttp.ToHttpClient());
+                HttpClient subject = new HttpClient(mockHandler.ToHttpClient());
 
                 IHttpResponse response = await subject.SendAsync(request);
 
-                mockHttp.VerifyNoOutstandingRequest();
+                Assert.NotNull(response);
+
+                handlerMock.Protected().Verify("SendAsync",
+                    Times.Once(),
+                    ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Get
+                    && req.RequestUri == endpointUri),
+                    ItExpr.IsAny<CancellationToken>());
             }
         }
 
@@ -38,18 +50,15 @@ namespace HotPotato.Core.Http.Default
         public async void SendAsync_ResponseContainsContent_WithType()
         {
             string expectString = "{\"id\":\"string\"}";
-
             HttpContent expectContent = new StringContent(expectString, Encoding.UTF8, "application/json");
 
-            MockHttpMessageHandler mockHttp = new MockHttpMessageHandler();
-            mockHttp.When(AValidEndpoint)
-                .Respond(HttpStatusCode.OK).Respond(expectContent);
+            HttpMessageHandler mockHandler = GetMockHandler(HttpStatusCode.OK, expectString).Object;
 
             using (HttpRequest request = new HttpRequest(HttpMethod.Get, new Uri(AValidEndpoint)))
             {
                 request.SetContent(expectString);
 
-                HttpClient subject = new HttpClient(mockHttp.ToHttpClient());
+                HttpClient subject = new HttpClient(mockHandler.ToHttpClient());
 
                 IHttpResponse response = await subject.SendAsync(request);
                 string bodyString = response.ToBodyString();
