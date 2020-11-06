cb84f6c3be1a986f5fccab10a0926c0a9061b9f6
Timothy Cosgriff <Timothy.Cosgriff@hyland.com>
2020-11-06 17:25:25 +0000

    Merge pull request #2 in TATO/hot-potato from feature/AUTOTEST-151-create-test-suite-for-expiring-server-side-cookies to master
    Squashed commit of the following:
    
    commit 8ea740aa75b74f2eb46b52056ae76423594bfede
    Author: Nick Stefanski <Nick.Stefanski@hyland.com>
    Date:   Thu Oct 22 13:15:52 2020 -0400
    
        Added cookie to cookiejar
    
    commit c14ead18c1fe3b6d8b11e2dafe36e018a100ed9d
    Author: Nick Stefanski <Nick.Stefanski@hyland.com>
    Date:   Thu Oct 22 13:01:23 2020 -0400
    
        Created Cookies E2E test

---
+++ test/HotPotato.E2E.Test/CookiesTest.cs
@@ -0,0 +1,88 @@
+﻿using HotPotato.Core.Cookies;
+using HotPotato.Core.Http;
+using HotPotato.Core.Http.Default;
+using HotPotato.OpenApi.Results;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WireMock.Server;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace HotPotato.E2E.Test
+{
+    [Collection("Host")]
+    public class CookiesTest
+    {
+        private IWebHost host;
+
+        private const string ApiLocation = "http://localhost:5000";
+        private const string Endpoint = "/endpoint";
+        private const string ProxyEndpoint = "http://localhost:3232/endpoint";
+        private const string CookiesEndpoint = "http://localhost:3232/cookies";
+        private const string GetMethodCall = "GET";
+        private const string PlainTextContentType = "text/plain";
+        private const string ContentType = "Content-Type";
+
+        private const string CookieName = "TestCookie";
+        private const string CookieValue = "TestValue";
+        private const string CookiePath = "/";
+        private const string CookieDomain = "localhost";
+
+        public CookiesTest(HostFixture fixture)
+        {
+            host = fixture.host;
+        }
+
+        [Fact]
+        public async Task HotPotato_CookiesEndpoint_Should_Delete_Cookies()
+        {
+            var servicePro = host.Services;
+
+            //Setting up mock server to hit
+            const string expected = "ValidResponse";
+
+            using (var server = FluentMockServer.Start(ApiLocation))
+            {
+                server
+                    .Given(
+                        Request.Create()
+                            .WithPath(Endpoint)
+                            .UsingGet()
+                    )
+                    .RespondWith(
+                        Response.Create()
+                            .WithStatusCode(200)
+                            .WithHeader(ContentType, PlainTextContentType)
+                            .WithBody(expected)
+                    );
+
+                Core.Http.Default.HttpClient client = (Core.Http.Default.HttpClient)servicePro.GetService<IHttpClient>();
+
+                ICookieJar cookieJar = servicePro.GetService<ICookieJar>();
+                Cookie cookie = new Cookie(CookieName, CookieValue, CookiePath, CookieDomain);
+                cookieJar.Cookies.Add(cookie);
+
+                using (HttpRequest req = new HttpRequest(HttpMethod.Delete, new System.Uri(CookiesEndpoint)))
+                {
+                    IHttpResponse res = await client.SendAsync(req);
+                }
+
+                HttpMethod method = new HttpMethod(GetMethodCall);
+
+                IResultCollector resultCollector = servicePro.GetService<IResultCollector>();
+                resultCollector.Results.Clear();
+
+                using (HttpRequest req = new HttpRequest(method, new System.Uri(ProxyEndpoint)))
+                {
+                    IHttpResponse res = await client.SendAsync(req);
+                }
+
+                Assert.True(cookieJar.Cookies.Count == 0);
+            }
+        }
+    }
+}
