41ace8441ccdca9983e716cbb7fd90a6bf4a950f
davidmbillie <davidmbillie@gmail.com>
2021-08-09 12:26:45 -0400

    Misc changes that were obfuscated by formatting

--- src/HotPotato.Core/Http/HttpExtensions.cs
+++ src/HotPotato.Core/Http/HttpExtensions.cs
@@ -1,4 +1,4 @@
-﻿using HotPotato.Core.Http.Default;
+using HotPotato.Core.Http.Default;
 using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
@@ -11,215 +11,211 @@ using MSHTTP = Microsoft.AspNetCore.Http;
 
 namespace HotPotato.Core.Http
 {
-    public static class HttpExtensions
-    {
-        private const string customHeaderPrefix = "X-HP-";
-        private const string transferEncoding = "transfer-encoding";
-        private static readonly HashSet<string> MethodsWithPayload = new HashSet<string>
-        {
-            "POST",
-            "PATCH",
-            "PUT"
-        };
+	public static class HttpExtensions
+	{
+		private const string customHeaderPrefix = "X-HP-";
+		private static readonly HashSet<string> MethodsWithPayload = new HashSet<string>
+		{
+			"POST",
+			"PATCH",
+			"PUT"
+		};
 
-        private static readonly HashSet<string> ExcludedRequestHeaders =
-            new HashSet<string>
-            {
-                "connection",
-                "content-length",
-                "keep-alive",
-                "host",
-                "upgrade",
-                "upgrade-insecure-requests"
-            };
+		private static readonly HashSet<string> ExcludedRequestHeaders =
+			new HashSet<string>
+			{
+				"connection",
+				"content-length",
+				"keep-alive",
+				"host",
+				"upgrade",
+				"upgrade-insecure-requests"
+			};
 
-        private static readonly HashSet<string> ExcludedResponseHeaders =
-            new HashSet<string>
-            {
-                "connection",
-                "server",
-                "transfer-encoding",
-                "upgrade",
-                "x-powered-by"
-            };
+		private static readonly HashSet<string> ExcludedResponseHeaders =
+			new HashSet<string>
+			{
+				"connection",
+				"server",
+				"transfer-encoding",
+				"upgrade",
+				"x-powered-by"
+			};
 
-        /// <summary>
-        /// Convert from hot potato's request to a request message to be sent to the client
-        /// </summary>
-        /// <param name="this"></param>
-        /// <returns></returns>
-        public static HttpRequestMessage ToClientRequestMessage(this IHotPotatoRequest @this)
-        {
-            _ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
+		/// <summary>
+		/// Convert from hot potato's request to a request message to be sent to the client
+		/// </summary>
+		/// <param name="this"></param>
+		/// <returns></returns>
+		public static HttpRequestMessage ToClientRequestMessage(this IHotPotatoRequest @this)
+		{
+			_ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
 
-            HttpRequestMessage message = new HttpRequestMessage(@this.Method, @this.Uri);
-            if (@this.Content != null)
-            {
-                message.Content = @this.Content;
-            }
-            foreach (var item in @this.HttpHeaders)
-            {
-                if (!ExcludedRequestHeaders.Contains(item.Key.ToLowerInvariant()))
-                {
-                    if (!message.Headers.TryAddWithoutValidation(item.Key, item.Value) && message.Content != null)
-                    {
-                        message.Content.Headers.TryAddWithoutValidation(item.Key, item.Value);
-                    }
-                }
-            }
-            return message;
-        }
+			HttpRequestMessage message = new HttpRequestMessage(@this.Method, @this.Uri);
+			if (@this.Content != null)
+			{
+				message.Content = @this.Content;
+			}
+			foreach (var item in @this.HttpHeaders)
+			{
+				if (!ExcludedRequestHeaders.Contains(item.Key.ToLowerInvariant()) && !message.Headers.TryAddWithoutValidation(item.Key, item.Value) && message.Content != null)
+				{
+					message.Content.Headers.TryAddWithoutValidation(item.Key, item.Value);
+				}
+			}
+			return message;
+		}
 
-        /// <summary>
-        /// Convert from an mshtml response message to a hot potato request task to be resolved by the client
-        /// </summary>
-        /// <param name="this"></param>
-        /// <returns></returns>
-        public static async Task<IHotPotatoResponse> ToClientResponseAsync(this HttpResponseMessage @this)
-        {
-            _ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
+		/// <summary>
+		/// Convert from an mshtml response message to a hot potato request task to be resolved by the client
+		/// </summary>
+		/// <param name="this"></param>
+		/// <returns></returns>
+		public static async Task<IHotPotatoResponse> ToClientResponseAsync(this HttpResponseMessage @this)
+		{
+			_ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
 
-            HttpHeaders headers = new HttpHeaders();
-            if (@this.Headers != null)
-            {
-                foreach (var item in @this?.Headers)
-                {
-                    headers.Add(item.Key, item.Value);
-                }
-            }
+			HttpHeaders headers = new HttpHeaders();
+			if (@this.Headers != null)
+			{
+				foreach (var item in @this?.Headers)
+				{
+					headers.Add(item.Key, item.Value);
+				}
+			}
 
-            if (@this.Content != null)
-            {
-                foreach (var item in @this.Content?.Headers)
-                {
-                    headers.Add(item.Key, item.Value);
-                }
-                MediaTypeHeaderValue contentType = @this.Content.Headers?.ContentType;
+			if (@this.Content != null)
+			{
+				foreach (var item in @this.Content?.Headers)
+				{
+					headers.Add(item.Key, item.Value);
+				}
+				MediaTypeHeaderValue contentType = @this.Content.Headers?.ContentType;
 
-                contentType = contentType ?? new MediaTypeHeaderValue("application/json");
-                byte[] payload = await @this.Content.ReadAsByteArrayAsync();
+				contentType = contentType ?? new MediaTypeHeaderValue("application/json");
+				byte[] payload = await @this.Content.ReadAsByteArrayAsync();
 
-                return new HotPotatoResponse(@this.StatusCode, headers, payload, contentType);
-            }
-            else
-            {
-                return new HotPotatoResponse(@this.StatusCode, headers, new byte[] { }, new MediaTypeHeaderValue("application/json"));
-            }
-        }
+				return new HotPotatoResponse(@this.StatusCode, headers, payload, contentType);
+			}
+			else
+			{
+				return new HotPotatoResponse(@this.StatusCode, headers, Array.Empty<byte>(), new MediaTypeHeaderValue("application/json"));
+			}
+		}
 
-        /// <summary>
-        /// Converts the mshttp request to a hot potato request to be used by the proxy
-        /// </summary>
-        /// <param name="this"></param>
-        /// <param name="remoteEndpoint"></param>
-        /// <returns></returns>
-        public static async Task<IHotPotatoRequest> ToProxyRequest(this MSHTTP.HttpRequest @this, string remoteEndpoint)
-        {
-            _ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
-            _ = remoteEndpoint ?? throw Exceptions.ArgumentNull(nameof(remoteEndpoint));
+		/// <summary>
+		/// Converts the mshttp request to a hot potato request to be used by the proxy
+		/// </summary>
+		/// <param name="this"></param>
+		/// <param name="remoteEndpoint"></param>
+		/// <returns></returns>
+		public static async Task<IHotPotatoRequest> ToProxyRequest(this MSHTTP.HttpRequest @this, string remoteEndpoint)
+		{
+			_ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
+			_ = remoteEndpoint ?? throw Exceptions.ArgumentNull(nameof(remoteEndpoint));
 
-            HotPotatoRequest request = new HotPotatoRequest(new HttpMethod(@this.Method), @this.BuildUri(remoteEndpoint));
-            if (@this.Headers != null && @this.Headers.Count > 0)
-            {
-                foreach (var item in @this.Headers)
-                {
-                    if (!item.Key.Contains(customHeaderPrefix))
-                    {
-                        request.HttpHeaders.Add(item.Key, item.Value.ToArray());
-                    }
-                    else
-                    {
-                        request.CustomHeaders.Add(item.Key, item.Value.ToArray());
-                    }
-                }
-            }
-            if (MethodsWithPayload.Contains(@this.Method.ToUpperInvariant()) && @this.Body != null)
-            {
-                using (MemoryStream stream = new MemoryStream())
-                {
-                    await @this.Body.CopyToAsync(stream);
-                    if (!string.IsNullOrEmpty(@this.ContentType))
-                    {
-                        //Sanitize here since System.Net.Http was throwing a format exception
-                        //when sending POST/PUT requests with payloads through TestServer
-                        @this.ContentType = @this.ContentType.Split(";")[0];
-                    }
-                    request.SetContent(stream.ToArray(), @this.ContentType);
-                }
-            }
+			HotPotatoRequest request = new HotPotatoRequest(new HttpMethod(@this.Method), @this.BuildUri(remoteEndpoint));
+			if (@this.Headers != null && @this.Headers.Count > 0)
+			{
+				foreach (var item in @this.Headers)
+				{
+					if (!item.Key.Contains(customHeaderPrefix))
+					{
+						request.HttpHeaders.Add(item.Key, item.Value.ToArray());
+					}
+					else
+					{
+						request.CustomHeaders.Add(item.Key, item.Value.ToArray());
+					}
+				}
+			}
+			if (MethodsWithPayload.Contains(@this.Method.ToUpperInvariant()) && @this.Body != null)
+			{
+				using (MemoryStream stream = new MemoryStream())
+				{
+					await @this.Body.CopyToAsync(stream);
+					if (!string.IsNullOrEmpty(@this.ContentType))
+					{
+						//Sanitize here since System.Net.Http was throwing a format exception
+						//when sending POST/PUT requests with payloads through TestServer
+						@this.ContentType = @this.ContentType.Split(";")[0];
+					}
+					request.SetContent(stream.ToArray(), @this.ContentType);
+				}
+			}
 
-            return request;
-        }
+			return request;
+		}
 
-        /// <summary>
-        /// Allow responses to be asynchronously processed by hot potato 
-        /// </summary>
-        /// <param name="this"></param>
-        /// <param name="response"></param>
-        /// <returns></returns>
-        public static async Task ToProxyResponseAsync(this IHotPotatoResponse @this, MSHTTP.HttpResponse response)
-        {
-            _ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
-            _ = response ?? throw Exceptions.ArgumentNull(nameof(response));
+		/// <summary>
+		/// Allow responses to be asynchronously processed by hot potato 
+		/// </summary>
+		/// <param name="this"></param>
+		/// <param name="response"></param>
+		/// <returns></returns>
+		public static async Task ToProxyResponseAsync(this IHotPotatoResponse @this, MSHTTP.HttpResponse response)
+		{
+			_ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
+			_ = response ?? throw Exceptions.ArgumentNull(nameof(response));
 
-            response.StatusCode = (int)@this.StatusCode;
+			response.StatusCode = (int)@this.StatusCode;
 
-            if (@this.Headers != null)
-            {
-                foreach (var header in @this.Headers)
-                {
-                    if (!ExcludedResponseHeaders.Contains(header.Key.ToLowerInvariant()))
-                    {
-                        response.Headers.Add(header.Key, new StringValues(header.Value.ToArray()));
-                    }
-                }
-            }
+			if (@this.Headers != null)
+			{
+				foreach (var header in @this.Headers)
+				{
+					if (!ExcludedResponseHeaders.Contains(header.Key.ToLowerInvariant()))
+					{
+						response.Headers.Add(header.Key, new StringValues(header.Value.ToArray()));
+					}
+				}
+			}
 
-            if (@this.Content.Length > 0)
-            {
-                await response.Body.WriteAsync(@this.Content, 0, @this.Content.Length);
-            }
-        }
+			if (@this.Content.Length > 0)
+			{
+				await response.Body.WriteAsync(@this.Content);
+			}
+		}
 
-        public static Uri BuildUri(this MSHTTP.HttpRequest @this, string remoteEndpoint)
-        {
-            _ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
-            _ = remoteEndpoint ?? throw Exceptions.ArgumentNull(nameof(remoteEndpoint));
+		public static Uri BuildUri(this MSHTTP.HttpRequest @this, string remoteEndpoint)
+		{
+			_ = @this ?? throw Exceptions.ArgumentNull(nameof(@this));
+			_ = remoteEndpoint ?? throw Exceptions.ArgumentNull(nameof(remoteEndpoint));
 
-            return new Uri($"{remoteEndpoint}{@this.Path.Value}{@this.QueryString}");
-        }
+			return new Uri($"{remoteEndpoint}{@this.Path.Value}{@this.QueryString}");
+		}
 
-        public static string ToBodyString(this IHotPotatoResponse @this)
-        {
-            if (@this.Content == null || @this.ContentType == null)
-            {
-                return string.Empty;
-            }
+		public static string ToBodyString(this IHotPotatoResponse @this)
+		{
+			if (@this.Content == null || @this.ContentType == null)
+			{
+				return string.Empty;
+			}
 
-            byte[] bodyContent = @this.Content;
+			byte[] bodyContent = @this.Content;
 
-            Encoding encode = null;
-            switch (@this.ContentType.CharSet)
-            {
-                case "utf-8":
-                    encode = Encoding.UTF8;
-                    break;
-                case "utf-7":
-                #pragma warning disable
-                    encode = Encoding.UTF7;
-                #pragma warning restore 
-                    break;
-                case "utf-32":
-                    encode = Encoding.UTF32;
-                    break;
-                case "us-ascii":
-                    encode = Encoding.ASCII;
-                    break;
-                default:
-                    encode = Encoding.Default;
-                    break;
-            }
-            return encode.GetString(bodyContent);
-        }
-    }
+			Encoding encode = null;
+			switch (@this.ContentType.CharSet)
+			{
+				case "utf-8":
+					encode = Encoding.UTF8;
+					break;
+				case "utf-7":
+#pragma warning disable
+					encode = Encoding.UTF7;
+#pragma warning restore
+					break;
+				case "utf-32":
+					encode = Encoding.UTF32;
+					break;
+				case "us-ascii":
+					encode = Encoding.ASCII;
+					break;
+				default:
+					encode = Encoding.Default;
+					break;
+			}
+			return encode.GetString(bodyContent);
+		}
+	}
 }
