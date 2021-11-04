6764488cd5cc191a913d88836ab515980047822f
davidmbillie <davidmbillie@gmail.com>
2021-11-04 12:49:24 -0400

    JsonBodyValidatorExtensionsTest

---
+++ test/HotPotato.OpenApi.Test/Validators/BodyValidator/JsonBodyValidatorExtensionsTest.cs
@@ -0,0 +1,45 @@
+using System;
+using HotPotato.OpenApi.Models;
+using NJsonSchema;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HotPotato.OpenApi.Validators
+{
+	public class JsonBodyValidatorExtensionsTest
+	{
+		private const string AValidBody = "{'foo': 1}";
+		private const string AValidSchema = @"{'properties':{'foo':{'type':'integer'}}}";
+		private const string ABodyWithAnUnexpectedProperty = "{'bar': 2}";
+
+		[Fact]
+		public void ValidateUndefinedProperties_ReturnsEmptyList_WithValidSchema()
+		{
+			JsonSchema subject = JsonSchema.FromJsonAsync(AValidSchema).Result;
+
+			List<ValidationError> results = subject.ValidateUndefinedProperties(AValidBody);
+
+			Assert.Empty(results);
+		}
+
+		[Fact]
+		public void JsonBodyValidator_ReturnsAListWithCorrectValidationErrorKind_WithInvalidSchema()
+		{
+			JsonSchema subject = JsonSchema.FromJsonAsync(AValidSchema).Result;
+
+			List<ValidationError> results = subject.ValidateUndefinedProperties(ABodyWithAnUnexpectedProperty);
+
+			Assert.Equal(ValidationErrorKind.PropertyNotInSpec, results[0].Kind);
+		}
+
+		[Fact]
+		public void JsonBodyValidator_ReturnsAListWithCorrectValidationErrorKind_WithBlankSchema()
+		{
+			JsonSchema subject = new JsonSchema();
+
+			List<ValidationError> results = subject.ValidateUndefinedProperties(ABodyWithAnUnexpectedProperty);
+
+			Assert.Equal(ValidationErrorKind.PropertyNotInSpec, results[0].Kind);
+		}
+	}
+}
